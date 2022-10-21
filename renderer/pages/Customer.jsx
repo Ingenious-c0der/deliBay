1b75cbe58d196668540bb435a1f7b0be63f0eb21
orb-277 <omkarbhosale277@gmail.com>
2022-10-21 23:46:11 +0530

    CustomerTables

--- renderer/pages/Customer.jsx
+++ renderer/pages/Customer.jsx
@@ -1,27 +1,93 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
 
+function CustomerPreviousOrders(props){
+  const [category,setCategory] = useState([]);
+  useEffect(()=>{
+    const getCategory = async() => {
+      const res = await fetch('');
+      //JSON response for the api && id is in props
+      const getdata = await res.json();
+      setCategory(getdata);
+
+    };
+    getCategory();
+
+  },[]);
+
+  return(
+    <container>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer_id</th>
+            <th>Package_id</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {category.map((categoryinstance) => (
+            <tr key={categoryinstance.cus_id}>
+              <td>{categoryinstance.attribute1}</td>
+              <td>{categoryinstance.attribute2}</td>
+              <td>{categoryinstance.attribute3}</td>
+            </tr>
+          ))}
+
+          
+        </tbody>
+      </table>
+    </container>
+
+  );
+}
+
+
 
 function CustomerActiveOrders(props){
+  const [category,setCategory] = useState([]);
+  useEffect(()=>{
+    const getCategory = async() => {
+      const res = await fetch('');
+      //JSON response for the api && id in props
+      const getdata = await res.json();
+      setCategory(getdata);
+
+    };
+    getCategory();
+
+  },[]);
+
   return(
-    <div>
-        <h1>ActiveOrders {props.name}</h1>
-    </div>
+    <container>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer_id</th>
+            <th>Package_id</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {category.map((categoryinstance) => (
+            <tr key={categoryinstance.cus_id}>
+              <td>{categoryinstance.attribute1}</td>
+              <td>{categoryinstance.attribute2}</td>
+              <td>{categoryinstance.attribute3}</td>
+            </tr>
+          ))}
+
+          
+        </tbody>
+      </table>
+    </container>
 
   );
 }
 
-function CustomerPreviousOrders(props){
-  
-  return(
-    <div>
-        <h1>PreviousOrders {props.name}</h1>
-    </div>
 
-  );
-}
 
 function Customer() {
   const [cus_id_from_user,setData] = useState(null);
