fb276a4d871f932278504a313995c694fc747dbf
orb-277 <omkarbhosale277@gmail.com>
2022-10-21 10:02:47 +0530

    CSS and Pages

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
+
 function Home() {
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-javascript)</title>
+        <title>Home</title>
+        <link rel='stylesheet' href='Styles/loginchoice.css'></link>
       </Head>
       <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/next">
-            <a>Go to next page</a>
-          </Link>
-        </p>
-        <img src="/images/logo.png" />
+        <h1>Home</h1>
+        <Link href='/Admin'><div className='LoginChoice'><img className='LoginChoice-img' src='images/user.png' alt = 'admin'/><p>Admin</p></div></Link>
+        <Link href='/Customer'><div className='LoginChoice'><img className='LoginChoice-img' src='images/user.png' alt = 'Customer'/><p>Customer</p></div></Link>
+        <Link href='/Employee'><div className='LoginChoice'><img className='LoginChoice-img' src='images/user.png' alt = 'Employee'/><p>Employee</p></div></Link>
       </div>
     </React.Fragment>
   );
