dbcd1939e21525b6d1dc24c5feb7301328e115d3
INGENIOUS <sagarabhyankar18@gmail.com>
2022-10-24 12:58:03 +0530

    added dbInit

---
+++ main/helpers/dbInit.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '**************',
+    database: 'delibase'
+});
+connection.connect(function(err) {
+    // in case of error
+    //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+    //flush privileges;
+    // the above should make it work
+    if(err){
+        console.log("here 2");
+        console.log(err.code);
+        console.log(err.fatal);
+    }
+});
+
+exports.connection = connection;
